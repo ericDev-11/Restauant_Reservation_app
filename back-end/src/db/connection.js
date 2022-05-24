8a64a7a6fb83407723c35038bf55f2988102aa9c
Eric Eggleston <ete7493@gmail.com>
2022-05-24 12:51:33 -0400

    api corrected

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -2,5 +2,4 @@ const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
-//blah blah blah
 module.exports = knex;
